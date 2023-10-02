e5b852d8b07f9d38b3cb766e3e1d6e4d95d736ad
Adarsh <kpadarsh6801@gmail.com>
2023-10-02 21:32:29 +0530

    completed the new todos

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, } from '@nestjs/common';
 import { TodoService } from './todo.service';
 @Controller('todo')
 
 
 export class TodoController {
     constructor(private tudoService:TodoService) {}
+    // get all todo 
     @Get()
     getAllTodos(){
         return this.tudoService.getAllTodos()
     }
+    // for creating new todo 
+    @Post()
+    createNewTodo(@Body() data){
+        const {title, description}=data;
+       return this.tudoService.createTodo(title, description)
+    }
+
 }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { TodoEntity } from 'src/Entity/todo.entity';
+import { todo } from 'node:test';
+import { TodoEntity, TodoStatus } from 'src/Entity/todo.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -9,5 +10,15 @@ export class TodoService {
 
    async getAllTodos(){
         return await this.repo.find()
+    }
+
+    async createTodo(title:string, description:string){
+        const todo:TodoEntity= new TodoEntity();
+        todo.title=title;
+        todo.description=description;
+        todo.status=TodoStatus.OPEN;
+        
+        this.repo.create(todo);
+       return await this.repo.save(todo)
     }
 }
