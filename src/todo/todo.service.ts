b07e3b5d319f7167309d1275b4e12debb4b4c9c4
Adarsh <kpadarsh6801@gmail.com>
2023-10-02 13:34:02 +0530

    connected and solved the getallTodos problem

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TodoEntity } from 'src/Entity/todo.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+    constructor(@InjectRepository(TodoEntity) private readonly repo: Repository<TodoEntity>){}
+    
+   async getAllTudos(){
+        return await this.repo.find()
+    }
+}
