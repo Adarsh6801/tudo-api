59e78a710990eca7f5cd2d3c4956987bcfa8655b
Adarsh <kpadarsh6801@gmail.com>
2023-10-03 12:10:20 +0530

    user register completed

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterUserDto } from 'src/DTO/register-user.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+constructor(private readonly authService: AuthService){}
+
+    // for registration 
+    @Post('register')
+    registration(@Body(new ValidationPipe()) regDTO:RegisterUserDto){
+       return this.authService.registerUser(regDTO)
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RegisterUserDto } from 'src/DTO/register-user.dto';
+import { UserEntity } from 'src/Entity/user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcryptjs'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(@InjectRepository(UserEntity) private repo: Repository<UserEntity>) {}
+
+    // function for registering a user 
+    async registerUser(RegisterUserDto: RegisterUserDto) {
+        const { username, password } = RegisterUserDto;
+        const salt = await bcrypt.genSalt(12); // Generate a salt with a cost factor of 12
+        const hashed = await bcrypt.hash(password, salt); // Use the generated salt to hash the password
+        console.log(hashed);
+        const user:UserEntity= new UserEntity()
+        user.username=username;
+        user.password=hashed;
+        user.salt=salt;
+
+        this.repo.create(user)
+        try{
+            return await this.repo.save(user)
+        }catch(error){
+            throw new InternalServerErrorException('something went wrong, user was not created.')
+        }
+    }
+}
